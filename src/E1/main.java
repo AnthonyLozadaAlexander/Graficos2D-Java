fc9f8574c53e2ae9ce84ccebc67d1c69c826f5d1
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-12 03:17:30 -0500

    Agrega clase main para inicializar la interfaz de frmRectangulo

---
+++ src/E1/main.java
@@ -0,0 +1,12 @@
+package E1;
+
+import Concepto.FrmConcepto;
+
+public class main {
+    public static void main(String[] args) {
+        frmRectangulo window = new frmRectangulo();
+        window.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+    }
+}
