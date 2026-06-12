cec9a40b636813b022db1dc17ef02854a60564b0
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-12 03:16:57 -0500

    Agrega clase DibujoRectangulo para dibujar rectángulos en JPanel
    Esta clase extiende JPanel y sobrescribe el método paintComponent para dibujar un rectángulo negro en la posición especificada. Esto permite la visualización de formas en la interfaz gráfica.

---
+++ src/E1/DibujoRectangulo.java
@@ -0,0 +1,16 @@
+package E1;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DibujoRectangulo extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.BLACK);
+        g2d.drawRect(50, 100, 60, 40);
+    }
+}
